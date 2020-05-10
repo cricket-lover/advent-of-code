ef3955cf1f81fa5b8d28595e770c4031bf9b1a11
phaneendra <phaneendraandukuri@gmail.com>
2020-05-10 17:43:06 +0530

    modified day6 part1

--- day6.js
+++ day6.js
@@ -3,81 +3,34 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 let arrayOfData = data.split('\n');
 
-const resultObject = {};
-const resultArray = [];
+const orbits = {};
 
-arrayOfData.forEach((element) => {
-  const [key, value] = element.split(')');
-  resultObject[value] = key;
+arrayOfData = arrayOfData.map((e) => {
+  const values = e.split(')');
+  return values;
 });
 
-arrayOfData.forEach((element) => {
-  const [key, value] = element.split(')');
-  resultArray.push([value, key]);
-});
-
-const getValue = function (list, key) {
-  const keys = Object.keys(list);
-  if (!keys.includes(key)) {
-    return value;
-  }
-  value = list[key];
-  return getValue(list, value);
-};
-
-const mainRow = [];
-resultObject['B'] = -1;
-
-const getMainRow = function (list, key) {
-  const filtered = list.find((e) => e[1] === key);
-  if (filtered === undefined) {
+arrayOfData.forEach((e) => {
+  const keys = Object.keys(orbits);
+  if (keys.includes(e[1])) {
+    orbits[e[1]].push(e[0]);
     return;
   }
-  mainRow.push(filtered[0]);
-  return getMainRow(list, filtered[0]);
+  orbits[e[1]] = [e[0]];
+});
+
+const getTotalOrbitCount = (mappedOrbits, orbit) => {
+  if (orbit === 'COM') return 0;
+  const orbitData = mappedOrbits[orbit];
+  return getTotalOrbitCount(mappedOrbits, orbitData[0]) + orbitData.length;
 };
 
-getMainRow(resultArray, 'COM');
+//orbits is that object with key value pairs
+const orbitsList = Object.keys(orbits);
+const totalOrbits = orbitsList.reduce(
+  (total, orbit) => total + getTotalOrbitCount(orbits, orbit),
+  0
+);
+console.log('part one', totalOrbits);
 
-mainRow.forEach((e) => {
-  const temp = getValue(resultObject, e);
-  resultObject[e] = 1 + temp;
-});
 
-const mainValues = mainRow.map((e) => resultObject[e]);
-const mainSum = mainValues.reduce((e, c) => (c += e), 0);
-// const sum = function (context, element) {
-//   context += element;
-//   return context;
-// };
-
-// console.log(resultObject);
-
-for (const key in resultObject) {
-  if (resultObject.hasOwnProperty(key)) {
-    const value = getValue(resultObject, key);
-    resultObject[key] = 1 + value;
-  }
-}
-
-// let values = Object.values(resultObject);
-// values = values.map((e) => +e);
-// const total = values.reduce((e, c) => (c += e), 0);
-// console.log(total);
-// console.log(mainRow, mainValues);
-
-let arr = Object.values(resultObject);
-
-arr = arr.map((e) => {
-  let result = e;
-  if (typeof e == 'string') {
-    let some = e.replace('COM', 0);
-    some = some.split('');
-    some = some.map((e) => +e);
-    result = some.reduce((e, c) => (c += e), 0);
-  }
-  return result;
-});
-
-const total = arr.reduce((e, c) => (c += e), 0);
-console.log(total);
